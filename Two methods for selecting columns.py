f5d41d9f4e65e1a4cc406f4cc599bc79b9283417
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-12-21 16:58:23 +0600

    Update Two methods for selecting columns.py

--- Two methods for selecting columns.py
+++ Two methods for selecting columns.py
@@ -1,6 +1,6 @@
 # Select the column item from credit_records
 # Use brackets and string notation
-items = credit_records['location']
+items = credit_records['item']
 
 # Display the results
 print(items)
