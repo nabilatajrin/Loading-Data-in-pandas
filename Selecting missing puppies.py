4ef628fc42c9c6d2654db57797e9fd86bf12508b
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-12-22 16:39:35 +0600

    Update Selecting missing puppies.py

--- Selecting missing puppies.py
+++ Selecting missing puppies.py
@@ -3,9 +3,10 @@ greater_than_2 = mpr[mpr.Age > 2]
 print(greater_than_2)
 
 # Select the dogs whose Status is equal to Still Missing
-still_missing = mpr['Status']  == 'Still Missing'
+still_missing = mpr[mpr.Status == 'Still Missing']
 print(still_missing)
 
 # Select all dogs whose Dog Breed is not equal to Poodle
-not_poodle = mpr['Dog Breed'] != 'Poodle'
+not_poodle = mpr[mpr['Dog Breed'] != 'Poodle']
+
 print(not_poodle)
